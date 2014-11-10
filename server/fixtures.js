4508b081b53b43dc424dafe3f93988876dbd801d
Robert Brockie <robertbrockie@gmail.com>
2014-11-09 22:07:37 -0500

    Inital MVP. Crud ops on links with registration.

---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+// Test Data, not needed for now
+// if (Posts.find().count() === 0) {
+// 	Posts.insert({
+// 		title: 'Introducing Telescope',
+// 		url: 'http://sachagreif.com/introducing-telescope/'
+// 	});
+
+// 	Posts.insert({
+// 		title: 'Meteor',
+// 		url: 'http://meteor.com'
+// 	});
+
+// 	Posts.insert({
+// 		title: 'The Meteor Book',
+// 		url: 'http://themeteorbook.com'
+// 	});	
+// }
